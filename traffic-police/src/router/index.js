e28f1607781da5898a969e3b8650b91082390e42
shirunpei <shirunpei@chudaokeji.com>
2017-04-11 18:55:23 +0800

    便民服务以及一键挪车页面

--- traffic-police/src/router/index.js
+++ traffic-police/src/router/index.js
@@ -101,6 +101,37 @@ const routes = [
       title: '成绩'
     },
     component: require('../views/wschool/subclass/result.vue')
+  },
+  {
+    name: 'peopleWillCloud',
+    path: '/peopleWillCloud',
+    meta: {
+      title: '民意云'
+    },
+    component: require('../views/peopleWillCloud/peopleWillCloud.vue'),
+    redirect: '/peopleWillCloud/facility',
+    children: [
+      {
+        name: 'facility',
+        path: '/peopleWillCloud/facility',
+        component: require('../views/peopleWillCloud/child/facility.vue')
+      },
+      {
+        name: 'secure',
+        path: '/peopleWillCloud/secure',
+        component: require('../views/peopleWillCloud/child/secure.vue')
+      },
+      {
+        name: 'jam',
+        path: '/peopleWillCloud/jam',
+        component: require('../views/peopleWillCloud/child/jam.vue')
+      },
+      {
+        name: 'order',
+        path: '/peopleWillCloud/order',
+        component: require('../views/peopleWillCloud/child/order.vue')
+      }
+    ]
   }
 ]
 /* eslint-disable no-new */
--- traffic-police/src/views/queryLawless/child/queryTab.vue
+++ traffic-police/src/views/queryLawless/child/queryTab.vue
@@ -11,9 +11,7 @@
 </template>
 
 <script>
-
 export default {
-  
 }
 </script>
 
