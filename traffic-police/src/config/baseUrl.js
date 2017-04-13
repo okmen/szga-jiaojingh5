7b76d97be7c8dd2c409e0de38eb19d35bd5c7890
zhanshancheng <zhanshancheng@chudaokeji.com>
2017-04-13 16:04:21 +0800

    公共URL

--- traffic-police/src/config/baseUrl.js
+++ traffic-police/src/config/baseUrl.js
@@ -1,17 +1,18 @@
 // 添加URL需注释 哪个模块哪个接口
+const commonUrl = 'http://192.168.1.120:8100/'
 
-export const xstudy = 'http://192.168.1.120:8100/Classroom/StudyHomepage.html' // 消分学习接口 满分学习接口
+export const xstudy = `${commonUrl}Classroom/StudyHomepage.html` // 消分学习接口 满分学习接口
 
-export const answer = 'http://192.168.1.120:8100/Classroom/Study.html'  // 取题接口
+export const answer = `${commonUrl}Classroom/Study.html` // 取题接口
 
-export const grade = 'http://192.168.1.120:8100/Classroom/anserEnd.html'  // 成绩接口
+export const grade = `${commonUrl}Classroom/anserEnd.html` // 成绩接口
 
-export const facility = 'http://192.168.1.120:8100/convenience/equipmentDamageReport.html' // 设备损坏通报接口
+export const facility = `${commonUrl}convenience/equipmentDamageReport.html` // 设备损坏通报接口
 
-export const secure = 'http://192.168.1.120:8100/convenience/safeHiddenDanger.html' // 安全隐患通报接口
+export const secure = `${commonUrl}convenience/safeHiddenDanger.html` // 安全隐患通报接口
 
-export const jam = 'http://192.168.1.120:8100/convenience/trafficCongestion.html' // 交通拥堵通报接口
+export const jam = `${commonUrl}convenience/trafficCongestion.html` // 交通拥堵通报接口
 
-export const order = 'http://192.168.1.120:8100/convenience/sequenceChaos.html' // 秩序混乱通报接口
+export const order = `${commonUrl}convenience/sequenceChaos.html` // 秩序混乱通报接口
 
-export const moveCar = 'http://192.168.1.120:8100/convenience/oneKeyDodgen.html' // 一键挪车接口
+export const moveCar = `${commonUrl}convenience/oneKeyDodgen.html` // 一键挪车接口
