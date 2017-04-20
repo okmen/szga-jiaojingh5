a41869fc55bddfb4a1ab6f8a7fe802d2a92372f7
lizhenzhen <lizhenzhen@chudaokeji.com>
2017-04-20 20:26:54 +0800

    预约申办成功

---
+++ traffic-police/src/components/appointSuccess.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="appointSuccess">
+    <section class="appoint-img">
+      <dl>
+        <dd></dd>
+        <dt>申办成功</dt>
+      </dl>
+    </section>
+    <section class="appoint-box">
+      <h3>预约成功</h3>
+      <p>
+        您的信息已成功提交，我们将会在3个工作日内通过短信告知您的审核结果，您还可以凭身份证信息在深圳交警微信号中查询审核。
+      </p>
+      <p class="appoint-margin">业务类型：<span>机动车打刻原车发动机号码变更备案</span></p>
+      <p>流水号码：<span class="running-number">J5647988412564</span></p>
+    </section>
+    <div class="btn-appoint-backword">返回</div>
+  </div>
+</template>
+<style lang="less">
+#appointSuccess{
+  background:#FFF;
+  height:100%;
+  width:100%;
+  .appoint-img{
+    width:100%;
+    height:250px;
+    dl{
+      margin:0 auto;
+      padding-top:74px;
+      width:178px;
+      height:100%;
+      dd{
+        width:100%;
+        height:178px;
+        background:url('../images/appointSuccess.png') no-repeat;
+        background-size:100%;
+      }
+      dt{
+        line-height:72px;
+        text-align:center;
+      }
+    }
+  }
+  .appoint-box{
+    margin:100px auto 0;
+    padding:0 20px;
+    width:550px;
+    height:400px;
+    border:1px solid #969696;
+    border-radius:8px;
+    h3{
+      margin-bottom:30px;
+      width:100%;
+      height:70px;
+      border-bottom:1px solid #969696;
+      text-align:center;
+      line-height:70px;
+      font-size:30px;
+      font-weight:normal;
+    }
+    p{
+      font-size:24px;
+      line-height:40px;
+      .running-number{
+        color:#FF0000;
+      }
+    }
+    .appoint-margin{
+      margin-top:34px;
+    }
+  }
+  .btn-appoint-backword{
+    margin:20px auto 0;
+    width:550px;
+    height:82px;
+    background:#2696dd;
+    color:#FFF;
+    font-size:30px;
+    line-height:80px;
+    text-align:center;
+    border-radius:8px;
+  }
+}
+</style>
