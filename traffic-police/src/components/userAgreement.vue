9203703dbb41fdfaa859a164fa673ad06a6bc661
liwang <liwang@chudaokeji.com>
2017-04-26 14:35:56 +0800

    微课堂学习须知

--- traffic-police/src/components/userAgreement.vue
+++ traffic-police/src/components/userAgreement.vue
@@ -55,19 +55,19 @@ export default {
           case 'sspjbzysx': // 随手拍举报注意事项
             this.$router.push('/takePicturesInform')
             break
-          case 'xfNotice': // 行人学习须知
+          case 'xfNotice': // 消分学习须知
             this.$router.push('/motorstudy#1')
             break
-          case 'xrNotice': // 行人学习须知
+          case 'mfNotice': // 满分学习须知
             this.$router.push('/answer#2')
             break
-          case 'mfNotice': // 行人学习须知
+          case 'abNotice': // ab类学习须知
             this.$router.push('/answer#3')
             break
-          case 'abNotice': // 行人学习须知
+          case 'ddcNotice': // 电动车学习须知
             this.$router.push('/answer#4')
             break
-          case 'ddcNotice': // 行人学习须知
+          case 'xrNotice': // 行人学习须知
             this.$router.push('/answer#5')
             break
         }
