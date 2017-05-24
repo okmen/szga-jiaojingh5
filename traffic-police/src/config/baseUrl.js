e77b6f20133f79c1c0ba0b9c8c817eca5254e0ac
wangguanwei <wangguanwei@chudaokeji.com>
2017-05-24 18:28:19 +0800

    违停免罚

--- traffic-police/src/config/baseUrl.js
+++ traffic-police/src/config/baseUrl.js
@@ -98,3 +98,5 @@ export const getTheChoiceOfIllegalActivities = `${commonUrl}user/getTheChoiceOfI
 export const getDOC = `${commonUrl}convenience/getAllResourcesAbsoluteUrl.html` // 获取民意云doc文档信息
 
 export const takePicturesQuery = `${commonUrl}user/search/queryResultOfReadilyShoot.html` // 随手拍举报查询接口
+
+export const illegalParkingAvoidFine = `http://192.168.1.71:8080/illegalHanding/illegalParkingAvoidFine.html` // 违停免罚接口
