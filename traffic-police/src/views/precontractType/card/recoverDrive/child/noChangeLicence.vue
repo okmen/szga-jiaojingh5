dd4a738ac2a10976590ca509141f6ecd6abcd363
lizhenzhen <lizhenzhen@chudaokeji.com>
2017-07-12 17:29:45 +0800

    驾驶证业务——恢复驾驶资格，满分学习，其它业务

---
+++ traffic-police/src/views/precontractType/card/recoverDrive/child/noChangeLicence.vue
@@ -0,0 +1,36 @@
+<!-- 
+*  #恢复驾驶资格#
+*  #逾期一年以上 * 未换证 * 类
+ -->
+<template>
+    <div class="noChangeLicence">
+      <common :orderPlaceData="appointPlaceData"
+              @appointTaskClick="appointTask"></common>
+      <div v-wechat-title="$route.meta.title"></div>
+    </div>
+  </div>
+</template>
+<script>
+import common from './common.vue'
+export default {
+  name: 'noChangeLicence',
+  data () {
+    return {
+      appointPlaceData: [   // 预约地点
+        { 'str': '深圳市车管分所' }
+      ]
+    }
+  },
+  components: {
+    common
+  },
+  methods: {
+    appointTask: function (params) {
+      console.log('未换证类', params)
+    }
+  }
+}
+</script>
+<style lang="less">
+
+</style>
