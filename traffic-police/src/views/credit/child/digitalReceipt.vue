21957ce25e303ac92c5c186ffd82abf779a5c003
liwang <liwang@chudaokeji.com>
2017-07-14 11:19:16 +0800

    电子回单

--- traffic-police/src/views/credit/child/digitalReceipt.vue
+++ traffic-police/src/views/credit/child/digitalReceipt.vue
@@ -5,6 +5,15 @@
         <li>深圳公安交通管理网上缴款缴费电子回单</li>
         <li>缴款编号 / 罚单编号：********** </li>
       </ul>
+      <div class="digit">
+        <h2>注:</h2>
+        <h3>
+          1、本回执不作为报销凭证，通过深圳交警星级用户平台网上支付、支付宝钱包、微信公众号、“深圳交警”APP等线上渠道
+        </h3>
+        <h3>
+          2、本回执不作为报销凭证，通过深圳交警星级用户平台网上支付、支付宝钱包、微信公众号、“深圳交警”APP等线上渠道
+        </h3>
+    </div>
     </div>
   </div>
 </template>
@@ -35,8 +44,20 @@ export default {
       li:first-of-type{
         color: #a7d9f9;
         font: 700 32px/70px '';
-        /*text-align: center;*/
         border-bottom: 1px solid #a7d9f9;
+      }
+      .digit{
+        width: 96%;
+        margin: 40px auto;
+        h2{
+          color: #8a8a8a;
+          font: 100 26px/32px '';
+        }
+        h3{
+          font: 100 26px/40px '';
+          text-indent: 30px;
+          color: #8a8a8a;
+        }
       }
     }
   }
