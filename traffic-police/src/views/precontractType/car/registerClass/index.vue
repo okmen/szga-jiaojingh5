0072e3f4e3e6bd61d7f6a9e485b456269ba5c9e9
wangguanwei <wangguanwei@chudaokeji.com>
2017-07-20 14:37:09 +0800

    更改下拉框

--- traffic-police/src/views/precontractType/car/registerClass/index.vue
+++ traffic-police/src/views/precontractType/car/registerClass/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="register-class">
-    <div-select :childInfo="businessType" @getSelected="getBusinessType" :defaultVal="defaultVal"></div-select>
+    <div class="form-template-item">
+    <span class="form-template-item-left">业务类型</span>
+    <div class="form-template-item-type">{{currentBusinessType}}</div>
+  </div>
     <div class="exchange-license-line"></div>
     <router-view :businessTypeId="businessTypeId" :modelOfCar="modelOfCar" :achieveCode="achieveCode"></router-view>
     <div v-wechat-title="$route.meta.title"></div>
@@ -9,6 +12,17 @@
 <style lang="less" scoped>
   .register-class > div {
     margin: 0 40px 30px;
+  }
+
+  .form-template-item-type {
+    border: 2px solid #e5e5e5;
+    border-radius: 8px;
+    outline: none;
+    height: 70px;
+    font-size: 30px;
+    padding-left: 15px;
+    line-height: 70px;
+    width: 68%;
   }
 
   .register-class {
@@ -28,7 +42,7 @@
 </style>
 <script>
   import {resultPost} from 'service/getData'
-//  import { MessageBox } from 'mint-ui'
+  //  import { MessageBox } from 'mint-ui'
   import {
     getBusinessTypeId
   } from 'config/baseUrl.js'
@@ -624,48 +638,18 @@
             }]
         }, // 车辆型号
         /* eslint-enable */
-        defaultVal: '',
         achieveCode: '',
+        currentBusinessType: '',
         businessType: {
-          title: '业务类型',
-          option: [
-            {
-              'str': '转移登记(市内过户)',
-              'id': 'transferRegistration'
-            },
-            {
-              'str': '机动车转移登记(转入)',
-              'id': 'intoRegister'
-            },
-            {
-              'str': '机动车注册登记',
-              'id': 'enteringRegister'
-            },
-            {
-              'str': '机动车注销登记',
-              'id': 'cancellationRegister'
-            },
-            {
-              'str': '机动车变更登记',
-              'id': 'changeRegister'
-            },
-            {
-              'str': '机动车变更登记(普通变更)',
-              'id': 'generalChangeRegister'
-            },
-            {
-              'str': '机动车变更登记(套牌车换证)',
-              'id': 'fakeLicensedEvidence'
-            },
-            {
-              'str': '机动车变更登记(夫妻变更)',
-              'id': 'manWifeChange'
-            },
-            {
-              'str': '补换领机动车行驶证',
-              'id': 'replaceLicense'
-            }
-          ]
+          'transferRegistration': '转移登记(市内过户)',
+          'intoRegister': '机动车转移登记(转入)',
+          'enteringRegister': '机动车注册登记',
+          'cancellationRegister': '机动车注销登记',
+          'changeRegister': '机动车变更登记',
+          'generalChangeRegister': '机动车变更登记(普通变更)',
+          'fakeLicensedEvidence': '机动车变更登记(套牌车换证)',
+          'manWifeChange': '机动车变更登记(夫妻变更)',
+          'replaceLicense': '补换领机动车行驶证'
         },
         businessTypeId: '',
         businessTypeStr: '',
@@ -682,65 +666,51 @@
         }
       }
     },
-    beforeRouteEnter (to, from, next) {
-      next(vm => {
-        vm.businessType.option.map(item => {
-          if (item.id === to.name) {
-            vm.defaultVal = item
-          }
-        })
-      })
-    },
-  /*  computed: {
-      responseData () {
-        return this.$store.getters.getResponseData
-      }
+    created () {
+      this.getBusinessTypeId()
     },
     watch: {
-      responseData (val) {
-        console.log(val, '返回的预约信息')
-        if (val.code === '0000') {
-          let dataInfo = {
-            type: 2,
-            textObj: {
-              businessType: this.businessTypeStr,
-              subscribeNo: val.data
-            }
-          }
-          this.$store.commit('saveSuccessInfo', dataInfo)
-          this.$router.push('/submitSuccess')
-        } else {
-          MessageBox('提示', val.data)
-        }
-      }
-    }, */
+      '$route': 'getBusinessTypeId'
+    },
+    /*  computed: {
+     responseData () {
+     return this.$store.getters.getResponseData
+     }
+     },
+     watch: {
+     responseData (val) {
+     console.log(val, '返回的预约信息')
+     if (val.code === '0000') {
+     let dataInfo = {
+     type: 2,
+     textObj: {
+     businessType: this.businessTypeStr,
+     subscribeNo: val.data
+     }
+     }
+     this.$store.commit('saveSuccessInfo', dataInfo)
+     this.$router.push('/submitSuccess')
+     } else {
+     MessageBox('提示', val.data)
+     }
+     }
+     }, */
     components: {
       divSelect: require('components/divSelect.vue')
     },
     methods: {
-      getBusinessTypeId (val) {
-        this.achieveCode = this.businessTypeToCode[val]
+      getBusinessTypeId () {
+        this.currentBusinessType = this.businessType[this.$router.currentRoute.name]
+        this.achieveCode = this.businessTypeToCode[this.$router.currentRoute.name]
         let requestData = {
           type: '1',
           part: '1',
-          code: this.businessTypeToCode[val]
+          code: this.businessTypeToCode[this.$router.currentRoute.name]
         }
         resultPost(getBusinessTypeId, requestData).then(data => {
           this.businessTypeId = data.data
           console.log(data, '业务类型编码获取成功')
         })
-      },
-      getBusinessType (val) {
-        /* this.businessType.option.map(item => {
-          if (item.id === val) {
-            this.businessTypeStr = item.str
-          }
-        }) */
-        this.$router.push(val)
-        this.businessTypeId = ''
-        if (this.defaultVal) {
-          this.getBusinessTypeId(val)
-        }
       }
     }
   }
