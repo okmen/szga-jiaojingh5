6b0258881741a198376ba0aac0adceb2291f619d
猪不乐意 <914230482@qq.com>
2017-07-24 16:25:34 +0800

    电动车扫码：对接扫码接口

--- traffic-police/src/views/electrocarFile/scanCode.vue
+++ traffic-police/src/views/electrocarFile/scanCode.vue
@@ -37,7 +37,7 @@
   }
 </style>
 <script>
-  import wx from 'weixin-js-sdk'
+  // import wx from 'weixin-js-sdk'
   export default {
     data () {
       return {
@@ -47,13 +47,16 @@
     methods: {
       /* eslint-disable */
       scanCode () {
-        wx.scanQRCode({
-          needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-          scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-          success: function (res) {
-            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-          }
+        this.$router.push({
+          name: 'electrocarFile'
         })
+        // wx.scanQRCode({
+        //   needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        //   scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        //   success: function (res) {
+        //     var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+        //   }
+        // })
       }
       /* eslint-enable */
     }
