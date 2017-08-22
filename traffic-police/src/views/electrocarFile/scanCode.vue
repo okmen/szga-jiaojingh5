b8cf3e53517f1825e866254dc688ce38cb191644
zhanshancheng <zhanshancheng@chudaokeji.com>
2017-08-22 10:59:16 +0800

    no message

--- traffic-police/src/views/electrocarFile/scanCode.vue
+++ traffic-police/src/views/electrocarFile/scanCode.vue
@@ -47,13 +47,13 @@
     methods: {
       /* eslint-disable */
       scanCode: function () {
-        // this.$router.push({path: '/electrocarFile', query: { billNo: '1234567890'}})
+        // this.$router.push({name: 'electrocarFile', query: { billNo: '440300000003'}})
         wx.scanQRCode({
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
-          success: function (res) {
+          success: (res) => {
             var result = res.resultStr // 当needResult 为 1 时，扫码返回的结果
-            this.$router.push({path: '/electrocarFile', query: { billNo: result.split(',')[1]}})
+            this.$router.push({name: 'electrocarFile', query: { billNo: result}})
           }
         })
       }
