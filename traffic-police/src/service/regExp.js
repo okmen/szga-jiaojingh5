6b449d516b91a551849df7052839ef369747063d
猪不乐意 <914230482@qq.com>
2017-09-14 15:59:26 +0800

    修改照片回执长度范围为10 ~ 25

--- traffic-police/src/service/regExp.js
+++ traffic-police/src/service/regExp.js
@@ -35,7 +35,7 @@ const plateNumberDetection = function (str) {
 
 // 检测照片回执码
 const isPhotoNum = function (str) {
-  var reg = /^[a-zA-Z0-9]{11,18}$/
+  var reg = /^[a-zA-Z0-9]{10,25}$/
   if (reg.test(str)) {
     return true;
   } else {
