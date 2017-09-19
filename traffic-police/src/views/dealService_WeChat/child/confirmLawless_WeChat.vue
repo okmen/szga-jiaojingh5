9f555a068087780a473b82af5969cb72e6d56eae
shirunpei <shirunpei@chudaokeji.com>
2017-09-19 13:52:33 +0800

    违法在线处理  权威标识 固定在页脚

--- traffic-police/src/views/dealService_WeChat/child/confirmLawless_WeChat.vue
+++ traffic-police/src/views/dealService_WeChat/child/confirmLawless_WeChat.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="confirmLawless-WeChat">
     <confirm-lawless></confirm-lawless>
-    <page-bottom></page-bottom>
+    <page-bottom :class="{positionBottom: positionBottom}"></page-bottom>
   </div>
 </template>
 
@@ -11,14 +11,28 @@ export default {
     'confirmLawless': require('../../newQueryLawless/child/confirmLawless.vue'),
     'pageBottom': require('../../../components/pageBottom.vue')
   },
+  data () {
+    return {
+      positionBottom: false
+    }
+  },
   mounted () {
     let isLogin = JSON.parse(window.localStorage.getItem('isLogin'))
     if (!isLogin) {
       this.$router.push('/login')
     }
+    this.$nextTick(function () {
+      this.positionBottom = document.getElementById('app').offsetHeight >= document.getElementsByClassName('confirmLawless-WeChat')[0].offsetHeight
+    })
   }
 }
 </script>
 
-<style lang="css">
+<style lang="less">
+  .positionBottom{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 </style>
