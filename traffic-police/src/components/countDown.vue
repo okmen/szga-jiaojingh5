a008e9c6f505ccddce6db5e1296890e33f5fed3d
liwang <liwang@chudaokeji.com>
2017-11-22 17:24:28 +0800

    提交倒计时组件

---
+++ traffic-police/src/components/countDown.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="countDown">
+    <div class="countDown-nav" id="oBox">
+      <img src="../images/itme/0.png" alt="">
+      <img src="../images/itme/0.png" alt="">天
+      <img src="../images/itme/0.png" alt="">
+      <img src="../images/itme/0.png" alt="">时
+      <img src="../images/itme/0.png" alt="">
+      <img src="../images/itme/0.png" alt="">分
+      <img src="../images/itme/0.png" alt="">
+      <img src="../images/itme/0.png" alt="">秒
+    </div>
+  </div>
+</template>
+<script>
+import { MessageBox } from 'mint-ui'
+export default {
+  /* eslint-disable */
+  name: 'countDown',
+  props: ['date'],
+  data () {
+    return {
+      date: '',
+      interval: ''
+    }
+  },
+  mounted () {
+    clearInterval(this.interval)
+    // this.date = new Date('2017/11/22 17:16:00')
+    this.interval = setInterval(this.time, 1000)
+  },
+  methods: {
+    toTwo: function (n) {
+      if(n > 9) {
+        return '' + n
+      } else
+      {
+        return '0' + n
+      }
+    },
+    time: function () {
+      let aImg = oBox.getElementsByTagName('img')
+      let mydate = new Date()
+      let str = ''
+      let t = this.date.getTime() - mydate.getTime()
+      if (t >= 0) {
+        str = this.toTwo(Math.floor(t/1000/60/60/24)) + this.toTwo(Math.floor(t/1000/60/60%24)) + this.toTwo(Math.floor(t/1000/60%60)) + this.toTwo(Math.floor(t/1000%60))
+        for(var i=0; i < aImg.length;i++) {
+          aImg[i].src= require('../images/itme/'+str[i]+'.png')
+        }
+      } else if (t <= 0) {
+        MessageBox({
+          title: '温馨提示',
+          message: '活动时间已结束'
+        }).then(action => {
+          clearInterval(this.interval)
+          this.$router.push('/')
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .countDown {
+    .countDown-nav {
+      margin: 20px auto;
+      text-align: center;
+      line-height: 74px;
+      img {
+        display: inline-block;
+        width: 48px;
+        height: 60px;
+        text-align: center;
+      }
+      img:nth-of-type(2n) {
+        padding-right: 8px;
+      }
+      img:nth-of-type(3) {
+        padding-left: 30px;
+      }
+      img:nth-of-type(5) {
+        padding-left: 30px;
+      }
+      img:nth-of-type(7) {
+        padding-left: 30px;
+      }
+    }
+  }
+</style>
