c60378b1670ab24c93b75575275beefe27ca7753
猪不乐意 <914230482@qq.com>
2018-03-28 14:55:43 +0800

    更新一键救援，隐藏刷脸修改手机号码入口

--- traffic-police/src/views/personalCenter/child/updateMobile.vue
+++ traffic-police/src/views/personalCenter/child/updateMobile.vue
@@ -48,7 +48,7 @@
     <input type="hidden" name="name" v-model="userName">
     <input type="hidden" name="pic_key" v-model="picKey">
     <input type="hidden" name="sig" :value="sig">
-    <button class="btn btn-custom"  type="button" name="button" @click.stop="sweepInto()">旧手机不在了</button>
+    <!-- <button class="btn btn-custom"  type="button" name="button" @click.stop="sweepInto()">旧手机不在了</button> -->
   </form>
 </div>
 </template>
